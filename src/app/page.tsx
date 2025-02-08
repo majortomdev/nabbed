7dcdc6e7ba3a4d54812a1fc4e4446953dd9953d4
Joseph Kinahan <kinahanj25@gmail.com>
2025-02-08 16:51:56 +0000

    connected clerk to prima/db so now logged in user gens row in db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,39 @@
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ModeToggle";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import { Button } from "@/components/ui/button";
+// import { ModeToggle } from "@/components/ModeToggle";
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import { prisma } from "@/lib/prisma";
 
-export default function Home() {
-  console.log("HHHHHHHHHEy from front/main page");
+// export default async function Home() {
+//   await prisma
+//   return (
+//     <div>
+//           <SignedOut>
+//             <SignInButton mode="modal">
+//               <Button>Sign in
+//                 </Button>
+//             </SignInButton>
+//           </SignedOut>
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+//           <Button variant={"secondary"}>Click me
+//           </Button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+  await prisma
   return (
-    <div>
-          <SignedOut>
-            <SignInButton mode="modal">
-              <Button>Sign in
-                </Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <Button variant={"secondary"}>Click me
-          </Button>
+    <div className="m-4">
+      <h1>home page content</h1>
     </div>
   );
 }
